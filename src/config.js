e13d15c08801e8dda95477536d4efecafa05099f
TheRealGiovas <e1_giovas@hotmail.com>
2022-01-28 16:13:57 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'RUvaxxed'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'dev-1643404486644-64508019040013'
 
 function getConfig(env) {
   switch (env) {
